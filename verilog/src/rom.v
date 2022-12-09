0950968b7492ee0801b40ac358e641c3aa613e3b
mebukibu <masa1280g@gmail.com>
2022-12-09 13:30:52 +0900

    rom.v の変更 ram.v のパラメータ記述と合わせる

--- verilog/src/rom.v
+++ verilog/src/rom.v
@@ -2,15 +2,17 @@
 
 module rom #(
     parameter filename = "../data/data18/weight18_0.txt",
-    parameter integer width = 11,     // 2^11 = 2048 > 5*288 = 1440
-    parameter integer depth = 5*288
+    parameter integer dwidth = `data_len,
+    parameter integer awidth = 11,          // 2^11 = 2048 > 5*288 = 1440
+    parameter integer words = 5*288
   ) (
     input wire clk,
-    input wire [width - 1:0] addr,
-    output reg [`data_len - 1:0] q
+    input wire [awidth - 1:0] addr,
+    output reg [dwidth - 1:0] q
   );
 
-  reg [`data_len - 1:0] mem [0:depth - 1];
+  (* ram_style = "block" *)
+  reg [`data_len - 1:0] mem [0:words - 1];
 
   always @(posedge clk) begin
     q <= mem[addr];
