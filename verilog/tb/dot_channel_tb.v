8d02744b6a1d4929a704d70e9391c27d3353201c
mebukibu <masa1280g@gmail.com>
2022-12-20 21:29:26 +0900

    dot_channel.v の変更

--- verilog/tb/dot_channel_tb.v
+++ verilog/tb/dot_channel_tb.v
@@ -1,33 +1,30 @@
 `include "../data/num_data.v"
+`include "../data/state_layer_data.v"
 
 module dot_channel_tb ();
 
   reg clk;
   reg rst_n;
-  reg load;
-  reg [2:0] cs;
-  reg [288*`data_len - 1:0] d;
+  reg dc_load;
+  reg ws_load;
+  reg [3:0] cs;
+  reg [2:0] phase;
+  reg [36*`data_len - 1:0] d;
   wire valid;
   wire [`data_len - 1:0] q;
 
-  dot_channel dot_channel0 (clk, rst_n, load, cs, d, valid, q);
+  dot_channel dot_channel0 (clk, rst_n, dc_load, ws_load, cs, phase, d, valid, q);
 
   initial clk = 0;
   always #5 clk = ~clk;
 
   initial begin
     $dumpvars;
-    rst_n=0; load=0; cs=0; d={288{18'b0000_01_00_0000_0000}}; #10
+    rst_n=0; dc_load=0; ws_load=0; cs=0; phase=0; d={36{18'b0000_01_00_0000_0000}}; #10
     rst_n=1; #10
-    cs=1; #10
-    load=1; #10
-    #3000
-    #5000
-    //load=0;
+    cs=`LAYER0; ws_load=1; phase=0; #10
+    dc_load=1; #10
     #100
-    cs=2; #100
-    #3000
-    #5000
     $finish;
   end
 
