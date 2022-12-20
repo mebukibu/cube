3d6a766e9b8f831e2b6b3388b8fc6fdcf9a2d4c5
mebukibu <masa1280g@gmail.com>
2022-12-20 20:57:46 +0900

    weight_store.v の変更

--- verilog/tb/weight_store_tb.v
+++ verilog/tb/weight_store_tb.v
@@ -1,23 +1,34 @@
 `timescale 1 ns / 1 ps
 `include "../data/num_data.v"
+`include "../data/state_layer_data.v"
 
 module weight_store_tb ();
 
   reg clk;
-  reg [2:0] cs;
+  reg load;
+  reg [3:0] cs;
+  reg [2:0] phase;
   wire valid;
-  wire [288*`data_len - 1:0] q;
+  wire [36*`data_len - 1:0] q;
 
-  weight_store #("../data/data18/weight18_0.txt") weight0(clk, cs, valid, q);
+  weight_store #("../data/data162/weight162_0.txt") weight0(clk, load, cs, phase, valid, q);
 
   initial clk = 0;
   always #5 clk = ~clk;
 
   initial begin
     $dumpvars;
-    cs = 0; #40
-    cs = 1; #3000
-    cs = 5; #3000
+    load=0; cs=`LIDLE; phase=0; #10
+    cs=`LAYER0; #10
+    load=1; #10
+    #60
+    load=0; #10
+    load=1; phase=1; #10
+    #60
+    load=0; #10
+    load=1; cs=`LAYER2; phase=3; #10
+    #60
+    #50
     $finish;
   end
 
